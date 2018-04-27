9471ea9962416b4335ee6d458d325431e9e9eca0
Małgorzata Kapuścińska <m_kapuscinska1@o2.pl>
2018-04-27 12:15:31 +0200

    Ready App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import App from './App';
 
-/*ReactDOM.render(<App />, document.getElementById('root'));*/
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
